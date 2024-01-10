28de453e108788b1d1786df58be9f39d075689ac
Max S. Whitten <madmaxfz@gmail.com>
2024-01-10 07:43:21 -0600

    Continue to separate the data and graphics components of the project as well as enhance re-usability. Almost have the PlanetVisual nailed down...

--- sysbody_visual.py
+++ sysbody_visual.py
@@ -6,17 +6,16 @@
 # Modified by Max S. Whitten in order to address the "stripe" glitch
 
 import numpy as np
-from PIL import Image
 import vispy.visuals.transforms as tr
-from poliastro.bodies import Body, Sun
+from PIL import Image
 from astropy import units as u
+from starsys_model import SYS_DATA
 from sysbody_model import SimBody
 from vispy.geometry import MeshData
 from vispy.visuals.mesh import MeshVisual
 from vispy.visuals.filters.mesh import TextureFilter
 from vispy.visuals import CompoundVisual
 from vispy.scene.visuals import create_visual_node
-
 
 DEF_TEX_FNAME = "resources/textures/2k_5earth_daymap.png"
 
@@ -28,84 +27,8 @@ def get_texture_data(fname=DEF_TEX_FNAME):
         return im.copy()
 
 
-def _oblate_sphere(rows, cols, radius, offset):
-    """ Generate mesh for a sphere along with texture coordinates such that
-        the 'stripe' anomaly is dealt with.
-
-    Parameters
-    ----------
-    rows : int - number of rows (usually even)
-    cols : int - number of columns (if None, set to rows * 2)
-    radius : float - mean radius of sphere (if None, set to 1)
-    offset : bool - offset each row by a column width?
-
-    Returns
-    -------
-    MeshData - mesh description of sphere
-    array - texture coordinates for each vertex
-
-    """
-    # TODO: fix the verts and/or tex coords to remove 'stripe'
-    #       I think I finally figured it out! Just need to change the code!
-
-    verts = np.empty((rows + 1, cols + 1, 3), dtype=np.float32)
-    tex_coords = np.empty((rows + 1, cols + 1, 2), dtype=np.float32)
-    # compute vertices
-    phi = (np.arange(rows + 1) * np.pi / rows).reshape(rows + 1, 1)
-    s = radius * np.sin(phi)
-    verts[..., 2] = radius * np.cos(phi)
-    th = ((np.arange(cols + 1) * 2 * np.pi / cols).reshape(1, cols + 1))
-    if offset:
-        # rotate each row by 1/2 column
-        th = th + ((np.pi / cols) * np.arange(rows + 1).reshape(rows + 1, 1))
-
-    verts[..., 0] = s * np.cos(th)
-    verts[..., 1] = s * np.sin(th)
-    # print(verts.size)
-    verts = verts.reshape((cols + 1) * (rows + 1), 3)
-    # print(verts.shape)
-
-    # set easternmost vert coords to match westernmost vert coords
-    for row in range(rows):
-        verts[(cols + 1) * (row + 2) - 1] = verts[(cols + 1) * (row+1)]
-
-    # compute texture coordinates here
-    tex_coords[..., 0] = (th / (2 * np.pi))
-    tex_coords[..., 1] = phi / np.pi
-
-    # remove redundant vertices from north and south poles
-    verts = verts.reshape((rows + 1) * (cols + 1), 3)[cols + 1:rows * (cols + 2)]
-    tex_coords = tex_coords.reshape((rows + 1) * (cols + 1), 2)[cols + 1:rows * (cols + 2)]
-
-    # compute faces as triplets of vertex indices
-    faces = np.empty(((rows + 1) * (cols + 1) * 2, 3), dtype=np.uint32)
-    rowtemplate1 = (((np.arange(cols + 1).reshape(cols + 1, 1) +
-                      np.array([[1, 0, 0]])) % (cols + 1)) +
-                    np.array([[0, 0, cols]]))
-    rowtemplate2 = (((np.arange(cols + 1).reshape(cols + 1, 1) +
-                      np.array([[1, 0, 1]])) % (cols + 1)) +
-                    np.array([[0, cols, cols]]))
-    for row in range(rows):
-        start = row * (cols + 2)
-        # print((faces[start:start + cols]).shape, "\n", (rowtemplate1 + row * cols).shape, "\n")
-        faces[start:start + cols + 1] = rowtemplate1 + row * (cols + 1)
-        faces[start + cols + 2:start + 2 * cols + 3] = rowtemplate2 + row * (cols + 1)
-
-    # cut off zero-area triangles at top and bottom
-    faces = faces[cols + 2:-(cols + 2)]
-
-    # adjust for redundant vertices that were removed from top and bottom
-    vmin = cols + 1
-    faces[faces < vmin] = vmin
-    faces -= vmin
-    vmax = verts.shape[0] - 1
-    faces[faces > vmax] = vmax
-    return MeshData(vertices=verts, faces=faces), tex_coords
-
-
 def _latitude(rows, cols, radius, offset):
     verts = np.empty((rows+1, cols, 3), dtype=np.float32)
-    tex_coords = np.empty((rows + 1, cols, 2), dtype=np.uint32)
 
     # compute vertices
     phi = (np.arange(rows+1) * np.pi / rows).reshape(rows+1, 1)
@@ -117,11 +40,8 @@ def _latitude(rows, cols, radius, offset):
         th = th + ((np.pi / cols) * np.arange(rows+1).reshape(rows+1, 1))
     verts[..., 0] = s * np.cos(th)
     verts[..., 1] = s * np.sin(th)
-    tex_coords[..., 0] = th / 2 * np.pi
-    tex_coords[..., 1] = phi / np.pi
     # remove redundant vertices from top and bottom
-    verts = verts.reshape((rows + 1)*cols, 3)[cols-1:-(cols-1)]
-    tex_coords = tex_coords.reshape((rows + 1) * cols, 2)[cols - 1:-(cols - 1)]
+    verts = verts.reshape((rows+1)*cols, 3)[cols-1:-(cols-1)]
 
     # compute faces
     faces = np.empty((rows*cols*2, 3), dtype=np.uint32)
@@ -144,7 +64,46 @@ def _latitude(rows, cols, radius, offset):
     faces -= vmin
     vmax = verts.shape[0]-1
     faces[faces > vmax] = vmax
-    return MeshData(vertices=verts, faces=faces), tex_coords
+    return MeshData(vertices=verts, faces=faces)
+
+
+def _oblate_sphere(rows, cols, radius, offset):
+    verts = np.empty((rows + 1, cols + 1, 3), dtype=np.float32)
+    # compute vertices
+    phi = (np.arange(rows+1) * np.pi / rows).reshape(rows+1, 1)
+    s = radius[0] * np.sin(phi)
+    verts[..., 2] = radius[2] * np.cos(phi)
+    th = ((np.arange(cols + 1) * 2 * np.pi / cols).reshape(1, cols + 1))
+    if offset:
+        # rotate each row by 1/2 column
+        th = th + ((np.pi / cols) * np.arange(rows+1).reshape(rows+1, 1))
+
+    verts[..., 0] = s * np.cos(th)
+    verts[..., 1] = s * np.sin(th)
+    # remove redundant vertices from top and bottom
+    verts = verts.reshape((rows + 1) * (cols + 1), 3)[cols:-cols]
+    # compute faces
+    faces = np.empty((rows * (cols + 1) * 2, 3), dtype=np.uint32)
+    rowtemplate1 = (((np.arange(cols + 1).reshape(cols + 1, 1) +
+                      np.array([[1, 0, 0]])) % (cols + 1) +
+                    np.array([[0, 0, cols + 2]])))
+    rowtemplate2 = (((np.arange(cols + 1).reshape(cols + 1, 1) +
+                      np.array([[1, 0, 1]])) % (cols + 1) +
+                    np.array([[0, cols + 2, cols + 2]])))
+    for row in range(rows):
+        start = row * (cols + 1) * 2
+        faces[start:start+(cols + 1)] = rowtemplate1 + row * (cols + 1)
+        faces[start+(cols + 1):start+(cols + 1) * 2] = rowtemplate2 + row * (cols + 1)
+    # cut off zero-area triangles at top and bottom
+    faces = faces[cols:-cols]
+
+    # adjust for redundant vertices that were removed from top and bottom
+    vmin = cols
+    faces[faces < vmin] = vmin
+    faces -= vmin
+    vmax = verts.shape[0]-1
+    faces[faces > vmax] = vmax
+    return MeshData(vertices=verts, faces=faces)
 
 
 class PlanetVisual(CompoundVisual):
@@ -180,7 +139,7 @@ class PlanetVisual(CompoundVisual):
         Shading to use.
     """
 
-    def __init__(self, ref_simbod=None, radius=1.0, rows=10, cols=None, offset=False,
+    def __init__(self, refbod_name=None, radius=1.0, rows=10, cols=None, offset=False,
                  vertex_colors=None, face_colors=None,
                  color=(1, 1, 1, 1), edge_color=(0, 0, 1, 0.2),
                  shading=None, texture=None, method='oblate', **kwargs):
@@ -190,11 +149,13 @@ class PlanetVisual(CompoundVisual):
         self._pos = None
         self._radii = []
         self._texture_data = None
-        if ref_simbod is not None:
-            self._ref_sb = ref_simbod
-            if type(self._ref_sb) is SimBody:        # if SimBody defined, get radii
-                _body = self._ref_sb.body
-                self.pos = self._ref_sb.pos2bary
+        if refbod_name is not None:
+
+            if refbod_name in SYS_DATA.body_names:        # if SimBody defined, get radii
+                _body = SYS_DATA.get_body_data(body_name=refbod_name,
+                                               data_keys="body_obj")
+
+                # self.pos = self._ref_sb.pos2bary
                 if _body.R_mean.value != 0:
                     self._radii.extend([_body.R,
                                         _body.R_mean,
@@ -206,36 +167,50 @@ class PlanetVisual(CompoundVisual):
 
             if type(texture) == str:  # assume filename
                 self._texture_data = get_texture_data(fname=texture)
-            elif texture is not None:  # assume image
-                self._texture_data = texture
+            elif texture is None:  # assume image
+                self._texture_data = SYS_DATA.get_body_data(body_name=refbod_name,
+                                                            data_keys='tex_data')
             else:  # use default
-                self._texture_data = self._ref_sb.texture
+                self._texture_data = texture
 
         else:
             texture = get_texture_data(fname=DEF_TEX_FNAME)
             self._radii = [10000.0, 10000.0, 10000.0] * u.km     # default to 1.0
 
         if method == 'latitude':
-            _mesh, self._tex_coords = _latitude(rows, cols, radius, offset)
+            radius = self._radii[0]
+            self._mesh_data = _latitude(rows, cols, radius, offset)
             # print("Using 'latitude' method...")
         else:
-            _mesh, self._tex_coords = _oblate_sphere(rows, cols, radius, offset)
+            radius = self._radii
+            self._mesh_data = _oblate_sphere(rows, cols, radius, offset)
             # print("Using 'oblate' method...")
 
+        self._tex_coords = np.empty((rows + 1, cols + 1, 2), dtype=np.float32)
+        for row in np.arange(rows + 1):
+            for col in np.arange(cols + 1):
+                self._tex_coords[row, col] = [row / rows, col / cols]
+        self._tex_coords = self._tex_coords.reshape((rows + 1) * (cols + 1), 2)
+
         # print(self._tex_coords)
-        self._mesh = MeshVisual(vertices=_mesh.get_vertices(),
-                                faces=_mesh.get_faces(),
+        self._mesh = MeshVisual(vertices=self._mesh_data.get_vertices(),
+                                faces=self._mesh_data.get_faces(),
                                 vertex_colors=vertex_colors,
                                 face_colors=face_colors,
                                 color=color,
                                 shading=shading)
+        print("TEXTURE:\n", self._texture_data,
+              "\nTEXCOORD:\n", self._tex_coords,
+              )
         self._filter = TextureFilter(texture=self._texture_data,
                                      texcoords=self._tex_coords,
+                                     enabled=True,
+                                     # shape=(self._texture_data.height, self._texture_data.width)
                                      )
         self._mesh.attach(self._filter)
         if np.array(edge_color).any():
-            self._border = MeshVisual(vertices=_mesh.get_vertices(),
-                                      faces=_mesh.get_edges(),
+            self._border = MeshVisual(vertices=self._mesh_data.get_vertices(),
+                                      faces=self._mesh_data.get_edges(),
                                       color=edge_color, mode='lines')
         else:
             self._border = MeshVisual()
@@ -249,6 +224,10 @@ class PlanetVisual(CompoundVisual):
     def mesh(self):
         """The vispy.visuals.MeshVisual that used to fil in."""
         return self._mesh
+
+    @mesh.setter
+    def mesh(self, new_mesh):
+        self._mesh = new_mesh
 
     @property
     def border(self):
@@ -265,7 +244,7 @@ class PlanetVisual(CompoundVisual):
             self._pos = new_pos
             self.transform = tr.MatrixTransform()
             self.transform.translate(self._pos)
-            self._update()
+            self.update()
 
     @property
     def texture(self):
@@ -274,19 +253,20 @@ class PlanetVisual(CompoundVisual):
     @texture.setter
     def texture(self, new_texture=DEF_TEX_FNAME):
         self._texture_data = get_texture_data(fname=new_texture)
-        self._filter = TextureFilter(texture=self._texture_data,
-                                     texcoords=np.array(self._tex_coords),
+        self._filter = TextureFilter(self._texture_data,
+                                     np.array(self._tex_coords),
+                                     enabled=True,
                                      )
         self._mesh.attach(self._filter)
-        self._update()
+        self.update()
 
-    @property
-    def visible(self):
-        return self._visible
-
-    @visible.setter
-    def visible(self, new_visible=False):
-        self._visible = new_visible
+    # @property
+    # def visible(self):
+    #     return self._visible
+    #
+    # @visible.setter
+    # def visible(self, new_visible=False):
+    #     self._visible = new_visible
 
 
 Planet = create_visual_node(PlanetVisual)
@@ -313,7 +293,7 @@ def main():
     # view.add(skymap)
     skymap.visible = False
     bod = Planet(rows=36,
-                 texture=get_texture_data(DEF_TEX_FNAME),
+                 refbod_name="Earth",
                  method='oblate',
                  parent=view.scene,
                  visible=True,
