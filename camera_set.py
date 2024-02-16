9a46e18df12660ea2e1910e029570a7d765952b4
Max S. Whitten <madmaxfz@gmail.com>
2024-02-15 21:20:34 -0600

    Graphics working, basic GUI composed, now to tie in the signals/slots in the main thread... adding new class: CameraSet

---
+++ camera_set.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+class CameraSet:
+    """     This class contains and manages a set of camera objects.
+        The user may add camera objects in a list, and these cameras
+        can be used in various views within an application.
+    """
+    def __init__(self):
